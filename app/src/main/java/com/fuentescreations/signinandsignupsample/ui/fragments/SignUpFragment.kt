17a9c26d5d4625cf325ef9477a3d068891aa43b7
Gonzalo <fuentesgonzalo@live.com>
2021-03-30 22:26:50 -0300

    Sign Up Fragment design done and Action bar removed

---
+++ app/src/main/java/com/fuentescreations/signinandsignupsample/ui/fragments/SignUpFragment.kt
@@ -0,0 +1,12 @@
+package com.fuentescreations.signinandsignupsample.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.fuentescreations.signinandsignupsample.R
+
+class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
