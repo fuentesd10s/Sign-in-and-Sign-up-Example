ee86855e91b83a23312cf3fcf443595f6bf40f9f
Gonzalo <fuentesgonzalo@live.com>
2021-03-30 21:48:51 -0300

    First commit

---
+++ app/src/main/java/com/fuentescreations/signinandsignupsample/ui/fragments/SignInFragment.kt
@@ -0,0 +1,12 @@
+package com.fuentescreations.signinandsignupsample.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.fuentescreations.signinandsignupsample.R
+
+class SignInFragment : Fragment(R.layout.fragment_sign_in) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
