949ce86a7a13f9271bd50049653cd813f996f2d6
Gonzalo <fuentesgonzalo@live.com>
2021-03-31 20:07:15 -0300

    ForgotPassword Fragment done

--- app/src/main/java/com/fuentescreations/signinandsignupsample/ui/fragments/ForgotPasswordFragment.kt
+++ app/src/main/java/com/fuentescreations/signinandsignupsample/ui/fragments/ForgotPasswordFragment.kt
@@ -1,11 +1,15 @@
 package com.fuentescreations.signinandsignupsample.ui.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import com.fuentescreations.signinandsignupsample.R
 import com.fuentescreations.signinandsignupsample.databinding.FragmentForgotPasswordBinding
 import com.fuentescreations.signinandsignupsample.ui.application.BaseFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ForgotPasswordFragment : BaseFragment(R.layout.fragment_forgot_password) {
 
@@ -15,5 +19,46 @@ class ForgotPasswordFragment : BaseFragment(R.layout.fragment_forgot_password) {
         super.onViewCreated(view, savedInstanceState)
         binding= FragmentForgotPasswordBinding.bind(view)
 
+        binding.btnResetPassword.setOnClickListener { if (checkFields()) resetPassword() }
+    }
+
+    private fun resetPassword() {
+        showLoading()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            Thread.sleep(2000)
+
+            activity?.runOnUiThread {
+                removeLoading()
+
+                AlertDialog.Builder(requireContext())
+                    .setTitle("Email sent.")
+                    .setMessage("We sent the instruction to your email.")
+                    .setPositiveButton("Ok"){ d, _-> d.dismiss()}
+                    .show()
+            }
+        }
+    }
+
+    private fun checkFields(): Boolean {
+        binding.textInputLayoutEmail.error = null
+
+        val email = binding.etEmail.text.toString()
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.textInputLayoutEmail.error = "Invalid email"
+            return false
+        }
+
+        return true
+    }
+    private fun showLoading() {
+        binding.progressBar.visibility = View.VISIBLE
+        binding.btnResetPassword.visibility = View.GONE
+    }
+
+    private fun removeLoading() {
+        binding.progressBar.visibility = View.GONE
+        binding.btnResetPassword.visibility = View.VISIBLE
     }
 }
