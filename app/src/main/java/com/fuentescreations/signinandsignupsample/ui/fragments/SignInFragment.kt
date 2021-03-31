7f252f58a013664fb470be48fae6f140928d5ec3
Gonzalo <fuentesgonzalo@live.com>
2021-03-31 13:45:11 -0300

    Animations between fragments added

--- app/src/main/java/com/fuentescreations/signinandsignupsample/ui/fragments/SignInFragment.kt
+++ app/src/main/java/com/fuentescreations/signinandsignupsample/ui/fragments/SignInFragment.kt
@@ -1,13 +1,18 @@
 package com.fuentescreations.signinandsignupsample.ui.fragments
 
+import android.app.Activity
 import android.os.Bundle
 import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.annotation.MainThread
 import androidx.navigation.fragment.findNavController
 import com.fuentescreations.signinandsignupsample.R
 import com.fuentescreations.signinandsignupsample.databinding.FragmentSignInBinding
 import com.fuentescreations.signinandsignupsample.ui.application.BaseFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class SignInFragment : BaseFragment(R.layout.fragment_sign_in) {
 
@@ -15,7 +20,7 @@ class SignInFragment : BaseFragment(R.layout.fragment_sign_in) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding= FragmentSignInBinding.bind(view)
+        binding = FragmentSignInBinding.bind(view)
 
         binding.btnSignIn.setOnClickListener { if (checkFields()) signIn() }
         binding.btnSignInFb.setOnClickListener { signInWithFb() }
@@ -35,46 +40,56 @@ class SignInFragment : BaseFragment(R.layout.fragment_sign_in) {
 
     private fun signIn() {
         showLoading()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            Thread.sleep(2000)
+
+            (requireContext() as Activity).runOnUiThread {
+                removeLoading()
+
+                mToast("Sign In successfully!")
+            }
+        }
     }
 
-    private fun checkFields() : Boolean{
-        binding.textInputLayoutEmail.error=null
-        binding.textInputLayoutPassword.error=null
+    private fun checkFields(): Boolean {
+        binding.textInputLayoutEmail.error = null
+        binding.textInputLayoutPassword.error = null
 
-        val email=binding.etEmail.text.toString()
-        val password=binding.etPassword.text.toString()
+        val email = binding.etEmail.text.toString()
+        val password = binding.etPassword.text.toString()
 
-        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            binding.textInputLayoutEmail.error="Invalid email"
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.textInputLayoutEmail.error = "Invalid email"
             return false
         }
-        if (password.length < 6){
-            binding.textInputLayoutPassword.error="Invalid password"
+        if (password.length < 6) {
+            binding.textInputLayoutPassword.error = "Invalid password"
             return false
         }
 
         return true
     }
 
-    private fun showLoading(){
-        binding.progressBar.visibility=View.VISIBLE
-        binding.btnSignIn.visibility=View.GONE
+    private fun showLoading() {
+        binding.progressBar.visibility = View.VISIBLE
+        binding.btnSignIn.visibility = View.GONE
 
-        binding.btnSignIn.isEnabled=false
-        binding.btnSignInGoogle.isEnabled=false
-        binding.btnSignInFb.isEnabled=false
-        binding.tvSignUp.isEnabled=false
-        binding.tvForgotPassword.isEnabled=false
+        binding.btnSignIn.isEnabled = false
+        binding.btnSignInGoogle.isEnabled = false
+        binding.btnSignInFb.isEnabled = false
+        binding.tvSignUp.isEnabled = false
+        binding.tvForgotPassword.isEnabled = false
     }
 
-    private fun removeLoading(){
-        binding.progressBar.visibility=View.GONE
-        binding.tvSignUp.visibility=View.VISIBLE
+    private fun removeLoading() {
+        binding.progressBar.visibility = View.GONE
+        binding.btnSignIn.visibility = View.VISIBLE
 
-        binding.btnSignIn.isEnabled=true
-        binding.btnSignInGoogle.isEnabled=true
-        binding.btnSignInFb.isEnabled=true
-        binding.tvSignUp.isEnabled=true
-        binding.tvForgotPassword.isEnabled=true
+        binding.btnSignIn.isEnabled = true
+        binding.btnSignInGoogle.isEnabled = true
+        binding.btnSignInFb.isEnabled = true
+        binding.tvSignUp.isEnabled = true
+        binding.tvForgotPassword.isEnabled = true
     }
 }
