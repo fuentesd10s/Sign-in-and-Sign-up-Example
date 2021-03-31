ab1e3902f9cd6493a967a74ed123bf648bc9cfeb
Gonzalo <fuentesgonzalo@live.com>
2021-03-30 23:36:48 -0300

    Forgot Password Fragment design done

---
+++ app/src/main/java/com/fuentescreations/signinandsignupsample/ui/fragments/ForgotPasswordFragment.kt
@@ -0,0 +1,12 @@
+package com.fuentescreations.signinandsignupsample.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.fuentescreations.signinandsignupsample.R
+
+class ForgotPasswordFragment : Fragment(R.layout.fragment_forgot_password) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
